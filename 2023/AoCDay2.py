17830c338b06f863c27d99c4981884c202ad7f70
SWarrener <simon.warrener@protonmail.com>
2024-09-02 11:22:28 +0100

    Improving and commenting day 2 solution

--- 2023/AoCDay2.py
+++ 2023/AoCDay2.py
@@ -1,35 +1,34 @@
-with open("input2.txt") as f: #part 1
+# https://adventofcode.com/2023/day/2
+
+# For each round in each game check if it is possible. Any colour having above the 
+# specified number is impossible. Sum ids of possible games and return.
+def calculate_p1(games):
     total = 0
-    for line in f.readlines():
-        gameid = int(line[line.find(" ")+1:line.find(":")])
-        line = line[line.find(":")+1:]
+    for gameid, game in games:
         possible = True
-        for game in line.split(";"):
-            for colour in game.split(","):
+        for round in game:
+            for colour in round.split(","):
                 data = colour.strip().split(" ")
                 num = int(data[0])
                 colourid = data[1]
                 if colourid == "green" and num > 13 or colourid == "red" and num > 12 or colourid == "blue" and num > 14:
                     possible = False
                     break
-            if possible == False:
-                break
-        if possible == True:
+            if not possible: break
+        if possible:
             total += gameid
-    print(f"The answer to part 1 is {total}")
-        
-with open("input2.txt") as f: #part 2
+    return total
+
+# For each game find the largest value of red blue and green needed by any individual round.
+# times them together and return the sum of this value for all games.
+def calculate_p2(games):
     total = 0
-    for line in f.readlines():
-        red = 0
-        blue = 0
-        green = 0
-        line = line[line.find(":")+1:]
-        for game in line.split(";"):
-            for colour in game.split(","):
-                data = colour.strip().split(" ")
-                num = int(data[0])
-                colourid = data[1]
+    for _, game in games:
+        red, blue, green = 0, 0, 0
+        for round in game:
+            for colour in round.split(","):
+                num, colourid = tuple(colour.strip().split(" "))
+                num = int(num)
                 if colourid == "green" and num > green:
                     green = num
                 elif colourid == "red" and num > red:
@@ -37,4 +36,19 @@ with open("input2.txt") as f: #part 2
                 elif colourid == "blue" and num > blue:
                     blue = num
         total += red*blue*green
-    print(f"The answer to part 2 is {total}")            
+    return total
+
+# A list of (gameids, game data) is extracted from the input
+with open("input2.txt") as f:
+    games = []
+    for line in f.readlines():
+        gameid = int(line[line.find(" ")+1:line.find(":")])
+        game = line[line.find(":")+1:].strip().split(";")
+        games.append((gameid, game))
+
+# Run the functions and print the answers. 
+p1_answer = calculate_p1(games)
+print(f"The answer to part 1 is {p1_answer}")
+
+p2_answer = calculate_p2(games)
+print(f"The answer to part 2 is {p2_answer}")
