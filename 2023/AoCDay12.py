22aca1af2499a9504bae75880bf8102c2c41ca53
SWarrener <simon.warrener@protonmail.com>
2024-09-29 12:39:43 +0100

    commenting day 12 and day 14

--- 2023/AoCDay12.py
+++ 2023/AoCDay12.py
@@ -1,5 +1,10 @@
+#https://adventofcode.com/2023/day/12
 from functools import cache
 
+# Recursively look through each spring to see what arrangements can fit within the given string
+# Take the current number and look through the current bit of the string to see how many ways it
+# can fit in that series of '?' before hitting either a '.' or a '#', or reaching the limit of
+# space available for all subsequent number. Use cache as this problem can be memoised.
 @cache
 def recursive(springs, nums, result = 0):
     if not nums:
@@ -12,10 +17,12 @@ def recursive(springs, nums, result = 0):
             result += recursive(springs[nxt+1:], nums)
     return result
 
+# Extract a list of dictionaries, with a string representing the springs and a tuple of numbers
 with open("input12.txt") as f:
     Records = []
     for line in f.readlines():
-        Records.append({"Springs": line[:line.find(" ")].strip(), "nums": tuple(map(int, line[line.find(" "):].strip().split(",")))})
+        Records.append({"Springs": line[:line.find(" ")].strip(),
+                        "nums": tuple(map(int, line[line.find(" "):].strip().split(",")))})
 
 p1answer, p2answer = 0, 0
 
--- 2023/AoCDay14.py
+++ 2023/AoCDay14.py
@@ -1,9 +1,15 @@
+# https://adventofcode.com/2023/day/14
+
+# Inverts the grid, so  ((1, 2),(3, 4)) becomes ((1, 3),(2, 4))
 def InvertGrid(grid):
     return [[grid[i][j] for i in range(len(grid))] for j in range(len(grid[0]))]
 
+# Rotate the grid by 90 degrees clockwise so ((1, 2),(3, 4)) becomes ((3, 1),(4, 2))
 def RotateGrid90(grid):
     return [list(reversed(x)) for x in zip(*grid)]
 
+# Rolls the moveable rocks towards the top of the grid. Works by inverting the grid and
+# moving them to the left (start of each line), then inverting the grid back.
 def RollRocks(grid):
     grid = InvertGrid(grid)
     for line in grid:
@@ -11,7 +17,7 @@ def RollRocks(grid):
         for i, item in enumerate(line):
             if item == '.':
                 continue
-            elif item == '#':
+            if item == '#':
                 last_cube = i
                 sphere_count = 0
             elif item == 'O':
@@ -21,18 +27,22 @@ def RollRocks(grid):
     grid = InvertGrid(grid)
     return grid
 
+# Calculate the load on the grid
 def CalculateLoad(grid):
     load = 0
     for i, line in enumerate(grid):
         load += line.count('O') * (len(grid) - i)
     return load
 
+# Roll rocks 4 times, once in each direction (actually works by always rolling in the same
+# direction but rotating the grid each time)
 def OneCycle(grid):
-    for i in range(4):
+    for _ in range(4):
         grid = RollRocks(grid)
         grid = RotateGrid90(grid)
     return grid
 
+# Extracts a list of lists representing the grid
 with open("input14.txt") as f:
     grid = []
     for line in f.readlines():
@@ -40,6 +50,8 @@ with open("input14.txt") as f:
 
 print(f"The answer to part 1 is: {CalculateLoad(RollRocks(grid))}")
 
+# Keeps rolling the rocks until we find a grid pattern that it repeated. Once we have that 
+# use some modulo arithmetic to find the answer.
 keys = []
 
 while True:
