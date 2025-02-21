cfea41ad211ae10a159d267177276d938d7c396c
SWarrener <simon.warrener@protonmail.com>
2025-02-21 13:40:34 +0000

    neatening up main program script

--- CsharpAoC/Program.cs
+++ CsharpAoC/Program.cs
@@ -9,20 +9,20 @@ var year = Convert.ToInt32(config["year"]);
 var test = Convert.ToBoolean(Convert.ToInt32(config["test"]));
 
 static void RunDay(int day, int year, bool test = true) {
-    var path = year + "/AoCDay" + day + ".cs";
+    var path = $"{year}/AoCDay{day}.cs";
     if (!File.Exists(path)) {
         Console.Write($"That combination of year {year} and day {day} does not exist. Quitting.\n");
         Environment.Exit(1);
     }
-    var module = "CsharpAoC.Year" + year + ".Day" + day + ".Solution";
+    var module = $"CsharpAoC.Year{year}.Day{day}.Solution";
     var type = Type.GetType(module);
     if (type == null) Environment.Exit(1);
     if (Activator.CreateInstance(type) is not ISolver solver) return;
     string[] input;
     if (test) {
-        input = File.ReadAllLines(year + "/inputtest.txt");
+        input = File.ReadAllLines($"{year}/inputtest.txt");
     } else {
-        input = File.ReadAllLines(year + "/input" + day + ".txt");
+        input = File.ReadAllLines($"{year}/input{day}.txt");
     }
     Console.WriteLine($"The answer to part 1 is: {solver.Part1(input)}");
     Console.WriteLine($"The answer to part 2 is: {solver.Part2(input)}");
